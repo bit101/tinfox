c4964eaa508a3c2ae0fafc6dd709046afea230b8
Keith Peters <kp@bit-101.com>
2024-04-05 20:10:36 -0400

    updating todo

--- main.go
+++ main.go
@@ -4,8 +4,15 @@ package main
 import "github.com/bit101/tinpig2/templates"
 
 /*
+TODO:
 - cobra
+	- h
+	- v (?) verbose/expert modes
 - update old configs
+- change executable name to just tinpig (not tinpig2)
+- template sections, i.e.
+	- tinpig go would show go templates, tinpig js would show js templates, etc.
+	- tinpig kp user defined, fav templates or whatever.
 - ansi.Printf(theme.Header, ...) -> theme.Headerf(...), etc.
 */
 
